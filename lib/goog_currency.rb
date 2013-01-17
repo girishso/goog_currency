ed3964bcc5a8c677bb1061a1fb1e4fb8b06ad831
Girish S <girish.sonawane@gmail.com>
2013-01-17 16:19:33 +0530

    removed unncessary references to GoogCurrency

--- lib/goog_currency.rb
+++ lib/goog_currency.rb
@@ -7,7 +7,7 @@ module GoogCurrency
     from, to = meth.to_s.split("_to_")
     
     if from.nil? or to.nil?
-      raise GoogCurrency::NoMethodException, "GoogCurrency accepts methods in 'usd_to_inr' or 'gbp_to_usd' format"
+      raise NoMethodException, "GoogCurrency accepts methods in 'usd_to_inr' or 'gbp_to_usd' format"
     end
     
     response = RestClient.get("http://www.google.com/ig/calculator?hl=en&q=#{args.first}#{from.upcase}=?#{to.upcase}").body
@@ -29,7 +29,7 @@ module GoogCurrency
         }
         response_hash['rhs'].encode(Encoding.find('ASCII'), encoding_options).to_f
     else
-      raise GoogCurrency::Exception, "An error occurred: #{response_hash['error']}"
+      raise Exception, "An error occurred: #{response_hash['error']}"
     end
   end
   
